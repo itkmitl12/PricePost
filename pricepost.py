f0e308714fca543645fc08b2ddd44c183bb6c0cd
pachara <s5070076@kmitl.ac.th>
2014-12-16 15:50:01 +0700

    Update pricepost.py

--- pricepost.py
+++ pricepost.py
@@ -8,7 +8,7 @@ def w_exit():
 """Class for main window"""
 class PricePost(object):
     def __init__(price_post):
-        """Main window"""
+        """Main windowa"""
         price_post.root = Tk()
         price_post.root.title("PricePost, IT KMITL project")
         price_post.root.geometry("224x160")
